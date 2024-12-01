b001d43121287e25cdcbfcc8d7c25a9f8c580b79
Dyevo <ydavid99@gmail.com>
2024-12-01 20:17:37 +0000

    Initial commit

---
+++ tests/unit/test_cdk_alb_assnmt_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from cdk_alb_assnmt.cdk_alb_assnmt_stack import CdkAlbAssnmtStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_alb_assnmt/cdk_alb_assnmt_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = CdkAlbAssnmtStack(app, "cdk-alb-assnmt")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
